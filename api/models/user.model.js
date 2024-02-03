039368d11a29a7cf5717414bf95e845b9ea296dd
umerxgit2004 <umer.workplace174@gmail.com>
2024-02-03 12:17:37 +0500

    create user model

---
+++ api/models/user.model.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const userSchema= new mongoose.Schema({
+    username: {
+        type:String,
+        required: true,
+        unique: true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+
+}, {timestamps:true})
+
+//create model
+const User = mongoose.model('User',userSchema)
