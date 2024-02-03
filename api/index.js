4678652931451c749b7a7a8fb063abadbe92ca07
umerxgit2004 <umer.workplace174@gmail.com>
2024-02-03 16:19:02 +0500

    add a middleware and a function to handle error

--- api/index.js
+++ api/index.js
@@ -13,6 +13,16 @@ app.use(express.json())
 app.use('/api/user',userRoutes)
 app.use('/api/auth',authRoutes)
 
+app.use((err,req,res,next)=>{
+    const statusCode = err.statusCode || 500
+    const message = err.message || "Internal Server Error"
+    res.status(statusCode).json({
+        success:false,
+        statusCode,
+        message
+    })
+})
+
 
 mongoose
 .connect(process.env.MONGO)
