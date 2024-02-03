57edf3032be3405fd8f6a0e94b16dd0db362e02d
umerxgit2004 <umer.workplace174@gmail.com>
2024-02-03 16:04:20 +0500

    created signup and api routes

--- api/index.js
+++ api/index.js
@@ -2,8 +2,16 @@ import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
 import userRoutes from './routes/user.route.js'
+import authRoutes from './routes/auth.route.js'
 
 dotenv.config()
+const app = express()
+app.listen(3000, ()=>{
+    console.log("Server is running on port 3000")
+})
+app.use(express.json())
+app.use('/api/user',userRoutes)
+app.use('/api/auth',authRoutes)
 
 
 mongoose
@@ -15,10 +23,9 @@ mongoose
     console.log(error)
 })
 
-const app = express()
 
-app.listen(3000, ()=>{
-    console.log("Server is running on port 3000")
-})
+
+
 
 app.use('/api/user',userRoutes)
+app.use('/api/auth',authRoutes)
