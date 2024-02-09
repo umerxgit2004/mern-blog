87c0342e1882c9d39c70cb7d6d386bada2c60c2f
umerxgit2004 <umer.workplace174@gmail.com>
2024-02-09 12:33:41 +0500

    update header component with userdata

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,11 +1,15 @@
-import { Navbar, TextInput, Button, NavbarToggle } from 'flowbite-react';
+import { Navbar, TextInput, Button, NavbarToggle, Dropdown, Avatar } from 'flowbite-react';
 import {AiOutlineSearch} from 'react-icons/ai'
 import { FaMoon } from 'react-icons/fa';
 import {Link, useLocation} from 'react-router-dom';
+import {useSelector} from 'react-redux'
 
 
 function Header() {
     const path= useLocation().pathname
+    const { currentUser } = useSelector(state => state.user || {});
+    console.log(currentUser)
+
   return (
     <Navbar className='border-b-2'>
         <Link to="/" className='self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white'>
@@ -24,12 +28,38 @@ function Header() {
             <Button className='w-12 h-10 hidden sm:inline' color='gray' pill>
                 <FaMoon/>
             </Button>
-            <Link to="/sign-in">
+            {currentUser ? (
+                <Dropdown
+                arrowIcon={false}
+                inline
+                label={
+                    <Avatar alt='user' img={currentUser.profilePicture} rounded/>
+                }
+                >
+                <Dropdown.Header>
+                    <span className='block text-sm'>@{currentUser.username}</span>
+                    <span className='block text-sm font-medium truncate'>{currentUser.email}</span>
+                </Dropdown.Header>
+                <Link to= {'/dashboard?tab=profile'}>
+                    <Dropdown.Item>
+                        Profile
+                    </Dropdown.Item>
+                </Link>
+                <Dropdown.Divider/>
+                <Dropdown.Item>Sign Out</Dropdown.Item>
+
+                </Dropdown>
+            ):
+            (
+                <Link to="/sign-in">
                 <Button gradientDuoTone='purpleToBlue' outline>
                     Sign In
                 </Button>
 
             </Link>
+            )
+            }
+            
             <Navbar.Toggle/> {/*so that it is after button when screen small*/}
          </div>
             <Navbar.Collapse>
